dd59f11e23cbe325f6a471244f06e58cde1c573e
huiiwen <110077149+huiiwen@users.noreply.github.com>
2024-10-04 17:06:08 +1000

    Add files via upload

--- Visualisation 2/vega_lite_vis.js
+++ Visualisation 2/vega_lite_vis.js
@@ -2,3 +2,8 @@ var vg_1 = "map.json";
 vegaEmbed("#map", vg_1).then(function(result) {
 // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_2 = "bubblepot.json";
+vegaEmbed("#bubble", vg_2).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
