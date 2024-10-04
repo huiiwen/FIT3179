751b877e6c7ac5f8033fc3fd24735a3ff1728cec
huiiwen <110077149+huiiwen@users.noreply.github.com>
2024-10-04 17:43:25 +1000

    Update vega_lite_vis.js

--- Visualisation 2/vega_lite_vis.js
+++ Visualisation 2/vega_lite_vis.js
@@ -3,7 +3,7 @@ vegaEmbed("#map", vg_1).then(function(result) {
 // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_2 = "bubblepot.json";
+var vg_2 = "bubbleplot.json";
 vegaEmbed("#bubble", vg_2).then(function(result) {
 // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
