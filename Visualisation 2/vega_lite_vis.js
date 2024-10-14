855d309cb9243b3a25ea612ddadb09bd26b09583
huiiwen <110077149+huiiwen@users.noreply.github.com>
2024-10-15 06:11:28 +1100

    Add files via upload

--- Visualisation 2/vega_lite_vis.js
+++ Visualisation 2/vega_lite_vis.js
@@ -1,14 +1,19 @@
 var vg_1 = "map.json";
-vegaEmbed("#map", vg_1).then(function(result) {
+vegaEmbed("#map", vg_1, {"actions": false}).then(function(result) {
 // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_2 = "bubbleplot.json";
-vegaEmbed("#bubble", vg_2).then(function(result) {
+vegaEmbed("#bubble", vg_2, {"actions": false}).then(function(result) {
 // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_3 = "stackedareachart.json";
-vegaEmbed("#stackedarea", vg_3).then(function(result) {
+vegaEmbed("#stackedarea", vg_3, {"actions": false}).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "multiple_interactive.json";
+vegaEmbed("#multiple", vg_4, {"actions": false}).then(function(result) {
 // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
